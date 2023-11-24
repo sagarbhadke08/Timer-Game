5f140d3fd90ee1dc7eb0b3c5cb2fd7721570203e
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-11-24 19:11:35 +0530

    Used useState(' ') hook for setting i/p value

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,12 +3,16 @@ import { useState } from "react";
 export default function Player() {
 
  const [enteredPlayer, setEnteredPlayer]= useState('');
+
+ function handleChange(event){
+  setEnteredPlayer(event.target.value);
+ }
   return (
     <section id="player">
       <h2>Welcome unknown entity</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange}/>
+        <button >Set Name</button>
       </p>
     </section>
   );
