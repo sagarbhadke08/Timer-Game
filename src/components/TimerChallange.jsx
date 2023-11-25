189efd579eaeefdf5b52375f7679545cb9e9d411
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-11-25 17:33:44 +0530

    small change

--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -4,7 +4,7 @@ export default function TimerChallange({ title, targetTime }) {
         <section className="challenge">
             <h2>{title}</h2>
             <p className="challenge-time">
-                {targetTime} second {targetTime > 1 ? 's' : ''}
+                {targetTime} second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
                 <button>
