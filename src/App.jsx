6378faa322794066cb89425cf1bca77e812c3b70
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-11-25 16:56:39 +0530

    Added new component 2 App.jsx for displaying games

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Player from './components/Player.jsx';
-
+import TimerChallange from './components/TimerChallange.jsx';
 function App() {
   return (
     <>
       <Player />
       <div id="challenges"></div>
+      <TimerChallange title="Easy" targetTime={1}/>
+      <TimerChallange title="Not Easy" targetTime={5}/>
+      <TimerChallange title="Getting Tough" targetTime={10}/>
+      <TimerChallange title="Pros Only" targetTime={15}/>
     </>
   );
 }
